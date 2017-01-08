15938ce9a151c746e63eaba87a39a75b81070810
mrtcam77 <mrtcam77@gmail.com>
2017-01-08 12:18:55 -0500

    modified fizzbuzz.js file to include my solution to fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,15 @@
-// Place your FizzBuzz code here. 
+// using 'for' loop to declare 'i' as a number
+for (i = 1; i <= 20; i++) {
+  //   if number is divisible by 3 and 5, then log fizzbuzz
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+//   if the number is divisible by 3, log Fizz
+  if (i % 3 === 0) {
+    console.log("Fizz");
+//     if number is divisible by 5 the log Buzz
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+//     if none of the above conditions are met then log the number
+  } else console.log(i);
+}
